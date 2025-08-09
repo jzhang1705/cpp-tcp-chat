2c8c1eabf5917af0c24a2a8f4ca73c147e2543fd
Jeffrey Zhang <jeffreyzhang5@gmail.com>
2025-08-09 10:44:58 -0400

    Allows users to enter a name

--- client.cpp
+++ client.cpp
@@ -9,41 +9,65 @@
 void receive_loop(int socket_fd) {
     char buffer[1024];
     while (true) {
-        ssize_t bytes = recv(socket_fd, buffer, sizeof(buffer), 0);
-        if (bytes <= 0) break;
-
+        ssize_t bytes = recv(socket_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes <= 0) {
+            std::cout << "\nDisconnected from server.\n";
+            break;
+        }
         buffer[bytes] = '\0';
         std::cout << "\n" << buffer << "\n> " << std::flush;
     }
 }
 
 int main() {
+    // Prompt for name before connecting
+    std::cout << "Please enter a name: ";
+    std::string client_name;
+    std::getline(std::cin, client_name);
+
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd < 0) {
         perror("socket");
         return 1;
     }
 
-    sockaddr_in server_addr{
-        .sin_family = AF_INET,
-        .sin_port = htons(5555),
-    };
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(5555);
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(sock_fd);
+        return 1;
+    }
 
     if (connect(sock_fd, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
         perror("connect");
+        close(sock_fd);
         return 1;
     }
 
     std::cout << "Connected to server. Type /quit to exit.\n";
+
+    // Send client name to server
+    if (send(sock_fd, client_name.c_str(), client_name.size(), 0) < 0) {
+        perror("send");
+        close(sock_fd);
+        return 1;
+    }
+
+    // Start receiving messages in background thread
     std::thread(receive_loop, sock_fd).detach();
 
     std::string msg;
     while (true) {
         std::cout << "> ";
-        std::getline(std::cin, msg);
+        if (!std::getline(std::cin, msg)) break; // Handle EOF
         if (msg == "/quit") break;
-        send(sock_fd, msg.c_str(), msg.size(), 0);
+
+        if (send(sock_fd, msg.c_str(), msg.size(), 0) < 0) {
+            perror("send");
+            break;
+        }
     }
 
     close(sock_fd);
--- server.cpp
+++ server.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <thread>
 #include <mutex>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-std::vector<int> clients;
+// std::vector<int> clients;
+std::unordered_map<int, std::string> clients;
 std::mutex clients_mutex;
 
 void handle_client(int client_fd) {
@@ -17,26 +19,56 @@ void handle_client(int client_fd) {
     while (true) {
         ssize_t bytes = recv(client_fd, buffer, sizeof(buffer), 0);
         if (bytes <= 0) {
-            std::cerr << "Client disconnected\n";
+            {
+                std::scoped_lock lock(clients_mutex);
+                std::cerr << clients[client_fd] << " disconnected\n";
+                clients.erase(client_fd);
+            }
             close(client_fd);
-            std::scoped_lock lock(clients_mutex);
-            clients.erase(std::remove(clients.begin(), clients.end(), client_fd), clients.end());
             break;
         }
 
         buffer[bytes] = '\0';
-        std::string message = std::to_string(client_fd) + ' ';
+        std::string message = clients[client_fd] + ": ";
         message += buffer;
 
-        std::scoped_lock lock(clients_mutex);
-        std::cout<<message.c_str()<<'\n';
-        for (int other_fd : clients) {
-            if (other_fd != client_fd) {
-                send(other_fd, message.c_str(), message.size(), 0);
+        std::vector<int> targets;   
+        {
+            std::scoped_lock lock(clients_mutex);
+            std::cout << message << '\n';
+            for (auto& [other_fd, name] : clients) {
+                if (other_fd != client_fd) {
+                    targets.push_back(other_fd);
+                }
             }
+        }
+        for (int fd : targets) {
+            send(fd, message.c_str(), message.size(), 0);
         }
     }
 }
+
+bool get_client_name(int client_fd) {
+    char buffer[1024];
+    ssize_t bytes = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+    if (bytes <= 0) {
+        std::cerr << "No name, client " << client_fd << " disconnected\n";
+        close(client_fd);
+        return false;
+    }
+    buffer[bytes] = '\0';
+
+    std::string name(buffer);
+    name.erase(name.find_last_not_of(" \n\r\t") + 1); // trim
+
+    {
+        std::scoped_lock lock(clients_mutex);
+        clients[client_fd] = name;
+    }
+    return true;
+}
+
+
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -50,6 +82,9 @@ int main() {
         .sin_port = htons(5555),
         .sin_addr = { .s_addr = INADDR_ANY }
     };
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     if (bind(server_fd, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
         perror("bind");
@@ -72,12 +107,9 @@ int main() {
             continue;
         }
 
-        {
-            std::scoped_lock lock(clients_mutex);
-            clients.push_back(client_fd);
-        }
-
+    if (get_client_name(client_fd)) { // make it return bool
         std::thread(handle_client, client_fd).detach();
+    }
     }
 
     close(server_fd);
